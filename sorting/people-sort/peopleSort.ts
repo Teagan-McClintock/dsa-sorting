9855ef66b69ac61cfcd2071eff15aaf9f994eb70
Crystal Tran <c.t.tran95@gmail.com>
2024-03-01 15:37:11 -0500

    completed peopleSort and selectionSort

--- sorting/people-sort/peopleSort.ts
+++ sorting/people-sort/peopleSort.ts
@@ -2,8 +2,25 @@ type tPerson = {fname: string, lname: string};
 
 /** Sort array of persons in place (lname > fname). Returns array. */
 
+// const people = [
+//   {fname: "George", lname: "Orwell"},
+//   {fname: "Jane", lname: "Austen"},
+//   {fname: "Sylvia", lname: "Plath"},
+//   {fname: "Christopher", lname: "Hitchens"},
+//   {fname: "Jennifer", lname: "Orwell"},
+// ];
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return [];
+  return arr.sort((a, b) => {
+    if(a.lname < b.lname) return -1;
+
+    if(a.lname === b. lname){
+      if(a.fname < b.fname) return -1;
+      if(a.fname > b.fname) return 1;
+      return 0;
+    }
+    if(a.lname > b.lname) return 1;
+    return 0;
+  })
 }
 
 export { peopleSort };
--- sorting/selection-sort/selectionSort.ts
+++ sorting/selection-sort/selectionSort.ts
@@ -1,7 +1,20 @@
 /** Sort in-place and return sorted array. */
-
+/// [4, 3, 1 , 2]
+// min is 4
+// min = 1, index 2
 function selectionSort(nums: number[]): number[] {
-  return nums;
+  if(nums.length <= 1) return nums;
+  let min = nums[0]
+  let index = 0;
+  for(let i = 1; i < nums.length; i++){
+    if(min > nums[i]){
+      min = nums[i];
+      index = i;
+    }
+  }
+  nums[index] = nums[0];
+  nums[0] = min;
+  return nums = [nums[0], ...selectionSort(nums.slice(1))];
 }
 
 export { selectionSort };
